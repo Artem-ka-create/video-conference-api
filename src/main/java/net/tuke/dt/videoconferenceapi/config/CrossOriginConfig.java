7adeb18e6406622e3b304f01957043719f70a5de
Artem Petrenko <64548375+Artem-ka-create@users.noreply.github.com>
2024-01-24 21:46:50 +0100

    Update CrossOriginConfig.java

--- src/main/java/net/tuke/dt/videoconferenceapi/config/CrossOriginConfig.java
+++ src/main/java/net/tuke/dt/videoconferenceapi/config/CrossOriginConfig.java
@@ -16,7 +16,7 @@ public class CrossOriginConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry
                         .addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://192.168.31.196:3000","http://172.20.10.2:3000")
+                        .allowedOrigins("http://localhost:3000", "https://5f94-147-232-176-71.ngrok-free.app","http://172.20.10.2:3000")
                         .allowedHeaders("*")
                         .allowCredentials(true)
                         .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
